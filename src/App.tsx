7478514389f5f1cdfe571289a46ca75bec900ee3
Ruedi Steinmann <ruediste@gmail.com>
2022-11-04 21:20:51 +0100

    divide by max

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,27 @@ function doRoundtrip(canvas: HTMLCanvasElement) {
   const ffts = performRoundtrip();
   const ctx = canvas.getContext("2d")!;
   const imageData = ctx.createImageData(1024, 100);
-  //const max=ffts.map(x=>Math.max(x.)
+  const max = Math.max(
+    ...ffts.map((x) =>
+      x.reduce(
+        (p, e) => (p === undefined ? e : Math.max(p, e)),
+        Number.NEGATIVE_INFINITY
+      )
+    )
+  );
+  console.log(max);
 
   // Iterate through every pixel
   for (let y = 0; y < 100 && y < ffts.length; y++) {
     for (let x = 0; x < 1024; x++) {
       const i = 4 * (y * 1024 + x);
-      const value = ffts[y][x];
+      const value = (255 * ffts[y][x]) / max;
       imageData.data[i + 0] = value; // R value
       imageData.data[i + 1] = value; // G value
       imageData.data[i + 2] = value; // B value
       imageData.data[i + 3] = 255; // A value
     }
   }
-
-  // const imageData = ctx.createImageData(100, 100);
-
-  // Fill the array with RGBA values
-  // for (let i = 0; i < imageData.data.length; i += 4) {
-  //   // Percentage in the x direction, times 255
-  //   let x = ((i % 400) / 400) * 255;
-  //   // Percentage in the y direction, times 255
-  //   let y = (Math.ceil(i / 400) / 100) * 255;
-
-  //   // Modify pixel data
-  //   imageData.data[i + 0] = x; // R value
-  //   imageData.data[i + 1] = y; // G value
-  //   imageData.data[i + 2] = 255 - x; // B value
-  //   imageData.data[i + 3] = 255; // A value
-  // }
 
   // Draw image data to the canvas
   ctx.putImageData(imageData, 20, 20);
