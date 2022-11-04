35b030dda638b534cbce93d4e8648420b67b891d
Ruedi Steinmann <ruediste@gmail.com>
2022-11-04 14:53:15 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { startInput } from "./audioInput";
 import { startRecorderProcessorInput } from "./audioWorkletProcessor";
 import { startPlayback } from "./audioBufferPlayer";
 
 function App() {
-    return (
-        <div className="App">
-            <button onClick={() => startInput()}>Start Recording</button>
-            <button onClick={() => startRecorderProcessorInput()}>Start Recording</button>
-            <button onClick={() => startPlayback()}>Start Playback</button>
-            </div>
-    );
+  return (
+    <div className="App">
+      <button onClick={() => startRecorderProcessorInput()}>
+        Start Recording
+      </button>
+      <button onClick={() => startPlayback()}>Start Playback</button>
+    </div>
+  );
 }
 
 export default App;
