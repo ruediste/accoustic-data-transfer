d00dcfc4803825a680d7559b75984998a59a1520
Ruedi Steinmann <ruediste@gmail.com>
2022-11-04 17:03:46 +0100

    switch time to seconds

--- src/workletPlayback.ts
+++ src/workletPlayback.ts
@@ -3,22 +3,21 @@ let t2 = 0;
 
 export async function startWorkletPlayback() {
   const audioCtx = new window.AudioContext();
-  const f1 = ((2 * Math.PI) / audioCtx.sampleRate) * 261.63;
-  const f2 = ((2 * Math.PI) / audioCtx.sampleRate) * 329.63;
+  const sampleTime = 1 / audioCtx.sampleRate;
+  const f1 = 2 * Math.PI * 261.63;
+  const f2 = 2 * Math.PI * 329.63;
 
   await audioCtx.audioWorklet.addModule("output.worklet.js");
   const source = new AudioWorkletNode(audioCtx, "output.worklet");
   source.port.onmessage = (event: { data: number }) => {
-    console.log(event.data);
-
     for (let i = 0; i < event.data; i++) {
       const buffer = new Float32Array(4096);
       for (let i = 0; i < buffer.length; i++) {
         buffer[i] = Math.sin(t1 * f1);
         buffer[i] += Math.sin(t2 * f2);
         buffer[i] /= 2;
-        t1 += 1;
-        t2 += 1;
+        t1 += sampleTime;
+        t2 += sampleTime;
       }
       source.port.postMessage(buffer);
 
